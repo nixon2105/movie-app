ece1a594a91dbf3aa47434a6c2f981e074d0cd66
nixon2105 <radivilov1991@mail.ru>
2022-01-25 09:42:38 +0300

    feat: add function getMovie

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const API_KEY = '86bf43e';
+
+async function getMovie() {
+  const url = `https://www.omdbapi.com/?s=marvel&page=2&apikey=${API_KEY}`;
+
+  const response = await fetch(url);
+  const data = await response.json();
+  const arr = data.Search;
+  arr.forEach((element) => {
+    console.log(element.Year);
+  });
+  // return data;
+}
+
+getMovie();
