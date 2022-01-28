fd052857299daddf6d2ce59610a83405c2174b0b
nixon2105 <radivilov1991@mail.ru>
2022-01-28 09:36:20 +0300

    feat: add function createTemplate

--- js/script.js
+++ js/script.js
@@ -1,13 +1,30 @@
-const API_KEY = '86bf43e';
+const cards = document.querySelector('.cards');
 
-async function getMovie() {
+const createTemplate = (movieData) => {
+  return `
+            <div class="card-item">
+              <div class="card__name">${movieData.Title}</div>
+              <div class="card__poster">
+                <img src="./img/poster.jpg" alt="" />
+              </div>
+              <div class="card__year">2021</div>
+              <div class="card__rating">8.1</div>
+            </div>
+  `;
+};
+
+const API_KEY = '86bf43e';
+const getMovie = async () => {
   const url = `https://www.omdbapi.com/?s=marvel&page=2&apikey=${API_KEY}`;
 
   const response = await fetch(url);
   const data = await response.json();
-  const arr = data.Search;
-  arr.forEach((element) => {
-    console.log(element.Year);
+  const movieData = data.Search;
+  movieData.map((element) => {
+    cards.innerHTML += createTemplate(element);
   });
-  // return data;
-}
+};
+
+getMovie();
+
+// const movieData = getMovie().then((movie) => movie.Search);
