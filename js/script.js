e769643f62aeae79a77e06d267c275918baa22b3
nixon2105 <radivilov1991@mail.ru>
2022-01-29 11:08:04 +0300

    feat: add function showMovie

--- js/script.js
+++ js/script.js
@@ -1,8 +1,15 @@
+//get API
+
 const API_KEY = 'api_key=11a9e127bc9b30c53ac934c29e209fea';
 const URL_BASE = 'https://api.themoviedb.org/3';
 const API_IMG = 'https://image.tmdb.org/t/p/w500';
 const URL = URL_BASE + '/discover/movie?sort_by=popularity.desc&' + API_KEY;
+const URL_SEARCH = URL_BASE + '/search/movie?' + API_KEY;
 
+const search = document.querySelector('.header-search');
+const inputSearch = document.querySelector('.search-request');
+const btnSearch = document.querySelector('.search-btn');
+const clearSearch = document.querySelector('.clear-btn');
 const cards = document.querySelector('.cards');
 
 const createTemplate = (movieData) => {
@@ -29,10 +36,24 @@ const getMovie = async () => {
   const response = await fetch(URL);
   const data = await response.json();
   const movieData = data.results;
-  console.log(movieData);
+  showMovie(movieData);
+};
+
+getMovie();
+
+const showMovie = (movieData) => {
   movieData.slice(0, 8).map((element) => {
     cards.innerHTML += createTemplate(element);
   });
 };
 
-getMovie();
+btnSearch.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const searchMovie = inputSearch.value;
+  console.log(searchMovie);
+
+  if (searchMovie) {
+    console.log(getMovie(URL_SEARCH + '&query=' + searchMovie));
+  }
+});
